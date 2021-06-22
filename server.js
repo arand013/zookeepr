4d405d581531614f67759a65c309cac6ceebd2fa
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-06-21 17:49:08 -0700

    new get and id functions

--- server.js
+++ server.js
@@ -53,6 +53,10 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
+};
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 
@@ -63,3 +67,12 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
